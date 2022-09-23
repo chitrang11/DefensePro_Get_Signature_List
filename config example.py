ffb90f1a5596226f382ab22949aa2d7c4198403f
chitrang11 <chitrang.patel11@hotmail.com>
2022-09-23 15:33:06 -0400

    Output file with timestamp and retention time for files

--- config example.py
+++ config example.py
@@ -3,4 +3,4 @@ VISION_USER = "radware" # APSolute Vision username
 VISION_PASS = "radware" # APSolute Vision password
 DefensePro_MGMT_IP= "10.10.10.11" # DefensePro IP
 Signature_Profile_Name= "MSSP" # DefensePro Signature Profile Name
-FileRetention = 365   # File retention time
+FileRetention = 365   # File retention in Day
